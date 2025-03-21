4b114e64d6f19d90b49b945ed386b34b5f440dbb
Rahimjon <abduraximovrahimjon8@gmail.com>
2025-03-21 10:53:48 +0500

    init: start book library pj

---
+++ server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const router = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Mongoose is connected'))
+
+app.use('/', router)
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running at port 3000')
+})
