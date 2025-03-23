a21f269669a0b777d38d21bf19546e42f61bc9bc
Rahimjon <abduraximovrahimjon8@gmail.com>
2025-03-23 14:17:28 +0500

    add and search authors

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const router = require('./routes/index')
+const bodyParser = require('body-parser')
+// Routes
+const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 
@@ -21,7 +25,8 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Mongoose is connected'))
 
-app.use('/', router)
+app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running at port 3000')
